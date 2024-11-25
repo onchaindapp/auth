655413ab5be8cb89c65b3604cc82a4460c25cede
onchaindapp <aumancalligari@gmail.com>
2024-11-25 16:30:56 +0100

    Update PB32-connect.js

--- Script/PB32-connect.js
+++ Script/PB32-connect.js
@@ -1,14 +1,14 @@
 document.getElementById("connectButton").addEventListener("click", function () {
-  window.location.href = "http://onchaindapp.github.io/dev/login.html"
+  window.location.href = "http://onchaindapp.github.io/auth/login.html"
 });
 document.getElementById("connectButton2").addEventListener("click", function () {
-  window.location.href = "http://onchaindapp.github.io/dev/login.html"
+  window.location.href = "http://onchaindapp.github.io/auth/login.html"
 });
 document.getElementById("connectButton3").addEventListener("click", function () {
-  window.location.href = "http://onchaindapp.github.io/dev/login.html"
+  window.location.href = "http://onchaindapp.github.io/auth/login.html"
 });
 document.querySelectorAll(".connectButton4").forEach(element => {
   element.addEventListener("click", function () {
-    window.location.href = "http://onchaindapp.github.io/dev/login.html";
+    window.location.href = "http://onchaindapp.github.io/auth/login.html";
   });
 });
